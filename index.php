08e8a3a7914bab5a4ee60813407f995941972dc7
Jefferson Pathay <jeffersonpathay@gmail.com>
2023-04-04 20:33:19 +0800

    Variables and Constants

--- index.php
+++ index.php
@@ -1,10 +1,31 @@
 <?php 
 
-//	echo "hello, Jhong";
+//constants
+ define('NAME', 'Jhong');
+ define("hobbies", ["Photography", "Candle Making", "Mobile App Developing"]); //constant array
+
+
+//variables
+ // $name = "Jhong";
+ $age = 26;
+
+
+//overriding
+ // $name = "Jefferson";
+
+
+ // echo $name
+ // echo hobbies[0];
+
+
 
 /*
 Source:
-https://www.youtube.com/watch?v=ABcXbZLm5G8&list=PL4cUxeGkcC9gksOX3Kd9KPo-O68ncT05o&index=3
+https://www.youtube.com/watch?v=2CXme275t9k&list=PL4cUxeGkcC9gksOX3Kd9KPo-O68ncT05o&index=4
+
+https://www.w3schools.com/php/php_variables.asp
+
+https://www.w3schools.com/php/php_constants.asp
 */
 ?>
 
@@ -13,11 +34,16 @@ https://www.youtube.com/watch?v=ABcXbZLm5G8&list=PL4cUxeGkcC9gksOX3Kd9KPo-O68ncT
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>My First PHP File</title>
+	<title>PHP Tutorials</title>
 </head>
 <body>
 
-	<h1><?php echo 'hello, jhong'; ?></h1>
+	<h1>User Profile Page</h1>
+
+	<div><?php echo NAME; ?></div>
+	<div><?php echo $age; ?></div>
+	<div><?php echo hobbies[2]; ?></div>
+
 
 </body>
 </html>
