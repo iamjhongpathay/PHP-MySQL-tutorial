3ad36d328c0289c066f10e3daab74f29f95ad482
Jefferson Pathay <jeffersonpathay@gmail.com>
2023-04-04 19:54:04 +0800

    Your first php file

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+
+//	echo "hello, Jhong";
+
+/*
+Source:
+https://www.youtube.com/watch?v=ABcXbZLm5G8&list=PL4cUxeGkcC9gksOX3Kd9KPo-O68ncT05o&index=3
+*/
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>My First PHP File</title>
+</head>
+<body>
+
+	<h1><?php echo 'hello, jhong'; ?></h1>
+
+</body>
+</html>
+
