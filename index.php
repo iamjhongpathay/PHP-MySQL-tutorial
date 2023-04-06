ee2a70e43f71c4788068994dbadb53a333be2436
Jefferson Pathay <jeffersonpathay@gmail.com>
2023-04-06 18:01:59 +0800

    Variables and Constants

--- index.php
+++ index.php
@@ -6,17 +6,26 @@
 
 
 //variables
- // $name = "Jhong";
- $age = 26;
+ $name = "Jhong";	//String
+ $age = 26;			//Integer
+ $grade = 76.50;	//Float/Double
+ $isPresent = true;	//Boolean
+ $favColor = array("Red","Green","Gray"); //Array
 
 
 //overriding
  // $name = "Jefferson";
 
 
- // echo $name
- // echo hobbies[0];
-
+ echo "Name: $name";
+ echo "<br>";
+ echo "Age: $age";
+ echo "<br>";
+ echo "Grade: $grade";
+ echo "<br>";
+ echo "Attendance(1/2 = Yes/No): $isPresent";
+ echo "<br>";
+ echo "Favorite Colors: ".$favColor[0].", ".$favColor[1].", ".$favColor[2];
 
 
 /*
@@ -41,7 +50,6 @@ https://www.w3schools.com/php/php_constants.asp
 	<h1>User Profile Page</h1>
 
 	<div><?php echo NAME; ?></div>
-	<div><?php echo $age; ?></div>
 	<div><?php echo hobbies[2]; ?></div>
 
 
