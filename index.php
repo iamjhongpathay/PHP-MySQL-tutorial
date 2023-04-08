93becb1624293a1bb91966e622bd431ac7055f91
Jefferson Pathay <jeffersonpathay@gmail.com>
2023-04-08 17:50:31 +0800

    Multi-dimentional Array

--- index.php
+++ index.php
@@ -1,71 +1,58 @@
 <?php
 
-//INDEXED arrays
-	$peopleOne = ['greg', 'leo', 'fred'];	//new way
-	echo $peopleOne[1];
+//multi-dimensional INDEXED arrays
+	$blogsOne = [
+		['mario party', 'mario', 'lorem', 30],
+		['mario kart cheats', 'toad', 'lorem', 25],
+		['zelda hidden chests', 'link', 'lorem', 50 ]
+	];
+
+	print_r($blogsOne);	//print all
+
+echo "<br>";
+	print_r($blogsOne[1]);	//print specific array
+
+echo "<br>";
+	print_r($blogsOne[1][1]);	//print specific value from specific array
+
+echo "<br>"; echo "<br>";
+
+
+
+//multi-dimensional ASSOCIATIVE arrays (KEY & VALUE Pairs)
+	$blogsTwo = [
+		['title'=>'mario party', 'author'=>'mario', 'content'=>'lorem', 'likes'=>30],
+		['title'=>'mario kart cheats', 'author'=>'toad', 'content'=>'lorem', 'likes'=>25],
+		['title'=>'zelda hidden chests', 'author'=>'link', 'content'=>'lorem', 'likes'=>50]
+	];
+
+	echo $blogsTwo[2]['author'];	//print specific value using its key from specific array
 
 echo "<br>";
 
-	$peopleTwo = array('grey', 'lim');	//older way
-	echo $peopleTwo[1];
+	echo count($blogsTwo);
 
 echo "<br>";
-
-//merging the arrays
-	$allPeople = array_merge($peopleOne, $peopleTwo);
-	print_r($allPeople);
-
-echo "<br>";
-
-	$ages = [20, 30, 40, 50];
-
-	$ages[1] = 25;	//overwrite value
-
-	$ages[] = 60;	//add new value in array
-
-	array_push($ages, 70);	//another way to add new value in array
-
-	print_r($ages);
+	
+	$blogsTwo[] = ['title'=>'castle party', 'author'=>'peach', 'content'=>'lorem', 'likes'=>100];	//add new element
+	print_r($blogsTwo);
 
 echo "<br>";
-
-	echo count($ages);	//total length
-
-echo "<br>";
-
-
-
-//ASSOCIATIVE array (KEY & VALUE Pairs)
-	$colorsOne = ['apple'=>'red', 'banana'=>'yellow', 'grapes'=>'purple'];	//new way
-	echo $colorsOne['banana'];
-
-echo "<br>";
-
-	print_r($colorsOne);
-
-echo "<br>";
-
-	$colorsTwo = array('sky'=>'blue', 'leaf'=>'green');	//older way
-
-	$colorsTwo['wood'] = 'brown';	//add new value
-
-	$colorsTwo['leaf'] = 'black';	//overwrite the value using key
-	print_r($colorsTwo);
-
+	
+	$popped = array_pop($blogsTwo);	//removed the last element of array
+	print_r($popped);
 
 
 /*
 Source:
 
-https://www.youtube.com/watch?v=bWygRxrlD44&list=PL4cUxeGkcC9gksOX3Kd9KPo-O68ncT05o&index=7
+https://www.youtube.com/watch?v=G1iDSoAXyvM&list=PL4cUxeGkcC9gksOX3Kd9KPo-O68ncT05o&index=8
 
 https://www.w3schools.com/php/php_arrays.asp
 
-https://www.w3schools.com/php/php_arrays_indexed.asp
-
-https://www.w3schools.com/php/php_arrays_associative.asp
-
+https://www.w3schools.com/php/php_arrays_multidimensional.asp
 */
+
 ?>
 
 <!DOCTYPE html>
